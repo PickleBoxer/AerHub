67a6d9fac2edaa4b2c8705327b844d4a5b361ed5
Matic Vertačnik <matic.vertacnik@icloud.com>
2025-02-18 15:01:00 +0100

    includes the Filament color scheme and the required Tailwind plugins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="application-name" content="{{ config('app.name') }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -11,8 +12,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        @filamentStyles
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/css/app.css')
 
         <!-- Styles -->
         @livewireStyles
@@ -40,6 +42,8 @@
 
         @stack('modals')
 
+        @filamentScripts
         @livewireScripts
+        @vite('resources/js/app.js')
     </body>
 </html>
