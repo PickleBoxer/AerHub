e68a87cb82e4df19335b6a020aa1828a08c08eb6
Matic Vertačnik <matic.vertacnik@icloud.com>
2025-03-03 16:54:01 +0100

    Add shared network to Docker Compose and update API authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:api');
-
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:api');
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
